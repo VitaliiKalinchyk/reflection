3b886ccc285c33f1d998ee8134ceaad9e2463f3d
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-01 11:54:54 +0200

    Writable version

--- src/main/java/kory/karl/reflection/task01/ReflectionMethods.java
+++ src/main/java/kory/karl/reflection/task01/ReflectionMethods.java
@@ -7,46 +7,18 @@ public class ReflectionMethods {
     //return instance of java.lang.Math class
     public Math invokingPrivateConstructor() {
         Math math = null;
-        Class<Math> mathInstance = Math.class;
-        Constructor<Math> privateConstructor;
-        try {
-            privateConstructor = mathInstance.getDeclaredConstructor();
-            if (privateConstructor.trySetAccessible()) {
-                math = privateConstructor.newInstance();
-            }
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
         return math;
     }
 
     //invoke Utils class method "methodToInvoke" and return its result
     public int invokingPrivateMethod(Utils util) {
         int result = -1;
-        Class<? extends Utils> utilClass = util.getClass();
-        try {
-            Method methodToInvoke = utilClass.getDeclaredMethod("methodToInvoke");
-            if (methodToInvoke.trySetAccessible()) {
-                result = (int) methodToInvoke.invoke(util);
-            }
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
         return result;
     }
 
     //get value of private field "fieldToGet"
     public int gettingPrivateField(Utils util) {
         int result = -1;
-        Class<? extends Utils> utilClass = util.getClass();
-        try {
-            Field fieldToGet = utilClass.getDeclaredField("fieldToGet");
-            if (fieldToGet.trySetAccessible()) {
-                result = fieldToGet.getInt(util);
-            }
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
         return result;
     }
 }
--- src/main/java/kory/karl/reflection/task01/annotations/Annos.java
+++ src/main/java/kory/karl/reflection/task01/annotations/Annos.java
@@ -3,8 +3,6 @@ package kory.karl.reflection.task01.annotations;
 import java.lang.annotation.*;
 
 //use this annotation to implement @Repeatable for @Anno annotation
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+
 public @interface Annos {
-    Anno[] value();
 }
